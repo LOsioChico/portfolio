0455f9ad84fd829da0aa5f9a2c067652a145527c
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-11-29 18:49:37 -0500

    feat: Add ESLint and Prettier configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard-with-typescript',
+    'eslint-config-prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};

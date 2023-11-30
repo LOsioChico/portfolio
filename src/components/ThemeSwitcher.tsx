acd79dc219a71a8fcd94bf07f64cb0403f691336
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-11-30 16:28:51 -0500

    style: remove unnecessary code and add conditional rendering

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,9 +1,17 @@
+'use client';
+
 import { useTheme } from 'next-themes';
 import { FaMoon } from 'react-icons/fa';
 import { HiSun } from 'react-icons/hi';
+import { useEffect, useState } from 'react';
 
 export const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   return (
     <button
@@ -12,10 +20,16 @@ export const ThemeSwitcher = () => {
         setTheme(theme === 'dark' ? 'light' : 'dark');
       }}
     >
-      {theme === 'dark' ? (
-        <HiSun className='text-2xl text-white' />
+      {mounted ? (
+        <span className='flex items-center justify-center gap-1 text-lg font-medium text-gray-900 dark:text-gray-100'>
+          {theme === 'dark' ? (
+            <HiSun className='text-2xl text-white' />
+          ) : (
+            <FaMoon className='text-2xl text-slate-700' />
+          )}
+        </span>
       ) : (
-        <FaMoon className='text-2xl text-slate-700' />
+        <div className='h-5 w-5 animate-pulse rounded-full bg-gray-200' />
       )}
     </button>
   );

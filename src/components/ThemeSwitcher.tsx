b6da7b136d0a95b18fbca781f699687a1126fb5f
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-11-30 17:28:24 -0500

    style: Increase size of ThemeSwitcher loading spinner

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -29,7 +29,7 @@ export const ThemeSwitcher = () => {
           )}
         </span>
       ) : (
-        <div className='h-5 w-5 animate-pulse rounded-full bg-gray-200' />
+        <div className='h-6 w-6 animate-pulse rounded-full bg-gray-200' />
       )}
     </button>
   );

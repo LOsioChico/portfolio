c7935870914cd4edf30c46e389b423ba55427e1d
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-11-30 15:09:00 -0500

    feat: Add LanguageSwitcher component to NavBar

---
+++ src/components/LanguageSwitcher.tsx
@@ -0,0 +1,22 @@
+import { locales } from '@/types/locale';
+import { useLocale } from 'next-intl';
+import Link from 'next/link';
+import { TbWorld } from 'react-icons/tb';
+
+export const LanguageSwitcher = () => {
+  const locale = useLocale();
+
+  const currentLocaleIndex = locales.findIndex((l) => l === locale);
+  const nextLocaleIndex =
+    currentLocaleIndex + 1 === locales.length ? 0 : currentLocaleIndex + 1;
+
+  return (
+    <Link
+      className='flex items-center justify-center gap-1 text-lg font-semibold text-gray-900 dark:text-gray-100'
+      href={`/${locales[nextLocaleIndex]}`}
+    >
+      <TbWorld className='text-2xl text-white' />
+      {locale.toUpperCase()}
+    </Link>
+  );
+};

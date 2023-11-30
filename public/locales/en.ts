3736c3993a6efc9768e192dfb5a478dff5947a7e
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-11-29 21:59:48 -0500

    fix(i18n): Update English and Spanish translations for experience

--- public/locales/en.ts
+++ public/locales/en.ts
@@ -5,6 +5,32 @@ const en: Locale = {
     author: 'Luis Osio Chico',
     description: 'Passionate front-end developer',
   },
+  experience: {
+    GrupoConsulTIC: {
+      title: 'Front-End Developer',
+      company: 'Grupo ConsulTIC',
+      description:
+        'Developing software applications related to virtual testers technology.*Collaborating with the team to design, develop, and implement new features and functionalities.*Participating in code reviews to contribute to continuous codebase improvement.',
+      start: 'Jun 2023',
+      end: 'Nov 2023',
+    },
+    UrbanMove: {
+      title: 'Front-End Developer | Contributing to Urban Move',
+      company: 'No Country',
+      description:
+        'Contributing to the development of "Urban Move," a revolutionary Uber-like platform for urban mobility.*Utilizing cutting-edge technologies such as TypeScript, React, Tailwind CSS, Prettier, ESLint, React-Icons, and Vite to craft an efficient and visually appealing platform.*Playing a pivotal role in the team\'s workflow, continuously reviewing and enhancing code to ensure top-notch quality and efficiency.*Preparing to take on testing responsibilities and code quality assurance with the upcoming implementation of Vitest.*This experience has enabled me to thrive professionally and enhance my technical skills, collaborating with a highly dedicated team focused on delivering a top-quality product.',
+      start: 'Aug 2023',
+      end: 'Sep 2023',
+    },
+    UVA: {
+      title: 'Front-End Developer | Contributing to UVA',
+      company: 'No Country',
+      description:
+        'Collaborating on UVA, a revolutionary platform connecting developers with exciting projects.*Crafting intuitive user interfaces with React and Tailwind CSS.*Utilizing Redux, RTK Query, and React Icons for efficient and visually appealing development.*Leveraging Vite for fast and flexible coding.*Ensuring code quality with eslint and prettier.',
+      start: 'Sep 2023',
+      end: 'Oct 2023',
+    },
+  },
 };
 
 export default en;
--- public/locales/es.ts
+++ public/locales/es.ts
@@ -5,6 +5,32 @@ const es: Locale = {
     author: 'Luis Osio Chico',
     description: 'Apasionado Desarrollador front-end',
   },
+  experience: {
+    GrupoConsulTIC: {
+      title: 'Desarrollador Front-End',
+      company: 'Grupo ConsulTIC',
+      description:
+        'Desarrollando aplicaciones de software relacionadas con la tecnología de probadores virtuales.*Colaborando con el equipo para diseñar, desarrollar e implementar nuevas funciones y funcionalidades.*Participando en revisiones de código para contribuir a la mejora continua del código base.',
+      start: 'Jun 2023',
+      end: 'Nov 2023',
+    },
+    UrbanMove: {
+      title: 'Desarrollador Front-End | Contribuyendo a Urban Move',
+      company: 'No Country',
+      description:
+        'Contribuyendo al desarrollo de "Urban Move", una plataforma revolucionaria similar a Uber para la movilidad urbana.*Utilizando tecnologías de vanguardia como TypeScript, React, Tailwind CSS, Prettier, ESLint, React-Icons y Vite para crear una plataforma eficiente y visualmente atractiva.*Jugando un papel fundamental en el flujo de trabajo del equipo, revisando y mejorando continuamente el código para garantizar una calidad y eficiencia de primer nivel.*Preparándome para asumir responsabilidades de prueba y garantía de calidad del código con la próxima implementación de Vitest.*Esta experiencia me ha permitido prosperar profesionalmente y mejorar mis habilidades técnicas, colaborando con un equipo altamente dedicado enfocado en ofrecer un producto de primera calidad.',
+      start: 'Aug 2023',
+      end: 'Sep 2023',
+    },
+    UVA: {
+      title: 'Desarrollador Front-End | Contribuyendo a UVA',
+      company: 'No Country',
+      description:
+        'Colaborando en UVA, una plataforma revolucionaria que conecta a los desarrolladores con proyectos emocionantes.*Creando interfaces de usuario intuitivas con React y Tailwind CSS.*Utilizando Redux, RTK Query y React Icons para un desarrollo eficiente y visualmente atractivo.*Aprovechando Vite para una codificación rápida y flexible.*Garantizar la calidad del código con eslint y prettier.',
+      start: 'Sep 2023',
+      end: 'Oct 2023',
+    },
+  },
 };
 
 export default es;

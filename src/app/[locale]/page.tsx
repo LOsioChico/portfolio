61d1405cbf353f2bacb758236b6093f38ad0e3f6
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-11-30 17:52:40 -0500

    feat(page, projects): Add Projects component and UVA project

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import { AnimatedContainer } from '@/components/containers/AnimatedContainer';
 import { Experience } from '@/components/containers/Experience';
 import { Profile } from '@/components/containers/Profile';
-// import { Projects } from '@/components/containers/Projects';
+import { Projects } from '@/components/containers/Projects';
 
 export default function Home() {
   return (
@@ -10,7 +10,8 @@ export default function Home() {
       <div />
       <hr className='rounded-md border border-gray-300 dark:border-gray-600' />
       <Experience />
-      {/* <Projects /> */}
+      <hr className='rounded-md border border-gray-300 dark:border-gray-600' />
+      <Projects />
     </AnimatedContainer>
   );
 }
--- src/components/containers/Projects.tsx
+++ src/components/containers/Projects.tsx
@@ -1,7 +1,14 @@
 import { PiFolderSimpleStar } from 'react-icons/pi';
-// import { Project } from '../project';
+import { Project } from '../project';
+import { useTranslations } from 'next-intl';
+import UVAImage from '@/assets/UVA.png';
 
 export const Projects = () => {
+  const t = useTranslations('projects');
+
+  const UVATitle = t('UVA.title');
+  const UVADescription = t('UVA.description');
+
   return (
     <section className='w-full'>
       <header className='mb-6 flex items-center gap-3 text-2xl font-bold text-indigo-400 dark:text-gray-100'>
@@ -9,7 +16,16 @@ export const Projects = () => {
         <PiFolderSimpleStar className='text-4xl' />
       </header>
       <div className='flex flex-col gap-10'>
-        <div className='grid gap-10'>{/* Projects */}</div>
+        <div className='grid gap-10'>
+          <Project
+            title={UVATitle}
+            description={UVADescription}
+            technologies={['React', 'JavaScript', 'TailwindCSS']}
+            image={UVAImage}
+            deployLink='https://uva.vercel.app'
+            sourceLink='https://github.com/No-Country/c12-14-ft-node-react'
+          />
+        </div>
       </div>
     </section>
   );
